4e624e551b07e6f414b32c9e34f3ba53b7c7c385
diddy500 <diddy.500@hotmail.com>
2015-07-21 08:23:39 -0300

    Done Tutorail 2

--- SDL-Game/main.cpp
+++ SDL-Game/main.cpp
@@ -3,6 +3,9 @@
 #include <string>
 #include "Cleanup.h"
 
+const int SCREEN_WIDTH = 640;
+const int SCREEN_HEIGHT = 480;
+
 void logSDLError(std::ostream &os, const std::string &msg)
 {
 	os << msg << " error: " << SDL_GetError() << std::endl;
@@ -43,69 +46,56 @@ void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, int x, int y)
 
 int main(int argc, char *argv[])
 {
-	//start SDL
-	if (SDL_Init(SDL_INIT_VIDEO) != 0)
+	if (SDL_Init(SDL_INIT_EVERYTHING) != 0)
 	{
-		std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
+		logSDLError(std::cout, "SDL_Init");
 		return 1;
 	}
 
-	//Create Window
-	SDL_Window* win = SDL_CreateWindow("Hello World!", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
-	if (win == nullptr)
+	SDL_Window* window = SDL_CreateWindow("Lesson 2", 100, 100, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+	if (window == nullptr)
 	{
-		std::cout << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
-		cleanup(win);
+		logSDLError(std::cout, "CreateWindow");
 		SDL_Quit();
 		return 1;
 	}
 
-	//Create Renderer
-	SDL_Renderer* ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
-	if (ren == NULL)
+	SDL_Renderer* renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+	if (renderer == nullptr)
 	{
-		cleanup(win, ren);
-		std::cout << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
+		logSDLError(std::cout, "CreateRenderer");
+		cleanup(window);
 		SDL_Quit();
 		return 1;
 	}
 
-	//Loading Bitmap
-	std::string imagePath = "res/hello.bmp";
-	SDL_Surface* bmp = SDL_LoadBMP(imagePath.c_str());
-	if (bmp == nullptr)
+	SDL_Texture *background = loadTexture("res/background.bmp", renderer);
+	SDL_Texture *image = loadTexture("res/image.bmp", renderer);
+	if (background == nullptr || image == nullptr)
 	{
-		cleanup(win, ren, bmp);
-		std::cout << "SDL_LoadBMP Error: " << SDL_GetError() << std::endl;
-		SDL_Quit();
-		return 1;
-	}
-	
-	//Createing Texture
-	SDL_Texture* tex = SDL_CreateTextureFromSurface(ren, bmp);
-	cleanup(bmp);
-	if (tex == nullptr)
-	{
-		std::cout << "SDL_CreateTextureFromSurface Error: " << SDL_GetError() << std::endl;
-		cleanup(win, ren, bmp, tex);
+		cleanup(background, image, renderer, window);
 		SDL_Quit();
 		return 1;
 	}
 
-	//Render Loop, waits 3 seconds
-	for (int i = 0; i < 10; ++i)
-	{
-		//Clearing Renderer
-		SDL_RenderClear(ren);
-		//Drawing texture
-		SDL_RenderCopy(ren, tex, NULL, NULL);
-		//Update screen
-		SDL_RenderPresent(ren);
-		//pause
-		SDL_Delay(1000);
-	}
+	SDL_RenderClear(renderer);
 
-	cleanup(win, ren, tex);
+	int bW, bH;
+	SDL_QueryTexture(background, NULL, NULL, &bW, &bH);
+	renderTexture(background, renderer, 0, 0);
+	renderTexture(background, renderer, bW, 0);
+	renderTexture(background, renderer, 0, bH);
+	renderTexture(background, renderer, bW, bH);
+
+	int iW, iH;
+	SDL_QueryTexture(image, NULL, NULL, &iW, &iH);
+	int x = SCREEN_WIDTH / 2 - iW / 2;
+	int y = SCREEN_HEIGHT / 2 - iH / 2;
+	renderTexture(image, renderer, x, y);
+
+	SDL_RenderPresent(renderer);
+	SDL_Delay(1000);
+
+	cleanup(background, image, renderer, window);
 	SDL_Quit();
-	return 0;
 }
